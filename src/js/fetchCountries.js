1e774a277afced71a8f2ab8274d030bfda23805e
YevheniiYadroch <evheniiyadroch@gmail.com>
2021-07-07 17:22:37 +0300

    completed

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,7 @@
 import countryDescriptionTpl from '../templates/countryDescription.hbs'
-
+import { alert, notice, info, success, error } from '@pnotify/core';
 const list = document.querySelector('.countries-list');
 const description = document.querySelector('.country');
-
 let markUp = '';
 
 export default function fetchCountries(searchQuery) {
@@ -13,11 +12,21 @@ export default function fetchCountries(searchQuery) {
         .then(countries => {
             list.innerHTML = '';
             description.innerHTML = '';
-            if (countries.length === 1) {
+            if (countries.length >= 10) {
+                const specifyNotice = alert({
+                 text: "Too many matches found. Please enter more specific query!"
+                });
+            } if (countries.length === 1) {
+                const success = alert({
+                 text: "Country found successfully!"
+                });
                 return description.insertAdjacentHTML('beforeend', countryDescriptionTpl(...countries));
             } if (countries.length > 1 & countries.length < 10) {
+                const specifyNotice = alert({
+                 text: "Too many matches found. Please enter more specific query!"
+                });
                 countries.forEach(country => {
-                    markUp += `<li class="country">${country.name}</li>`;
+                    markUp += `<li class="country-item">${country.name}</li>`;
                     list.insertAdjacentHTML('beforeend', markUp);
                     return markUp = '';
                 })
